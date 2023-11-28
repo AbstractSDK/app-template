75f034b52eee292a48aa08c95eeba6c85d41518e
adairrr <32375605+adairrr@users.noreply.github.com>
2023-11-28 13:58:22 +0100

    Install abstractjs and cosmos-kit

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,17 +1,25 @@
+import { AbstractProvider } from '@abstract-money/abstract.js-react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Layout from '~/lib/layout';
 import Routings from '~/lib/router/Routings';
 import { theme } from '~/lib/styles/theme';
 
+const queryClient = new QueryClient();
+
 const App = () => (
   <ChakraProvider theme={theme}>
-    <Router>
-      <Layout>
-        <Routings />
-      </Layout>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <AbstractProvider>
+        <Router>
+          <Layout>
+            <Routings />
+          </Layout>
+        </Router>
+      </AbstractProvider>
+    </QueryClientProvider>
   </ChakraProvider>
 );
 
