cb3c0077a6ede1d08738ec996aea0e17cf480630
Buckram <buckram123@gmail.com>
2024-06-03 15:05:06 +0300

    small fix

--- contracts/{{app_name | kebab_case}}/src/lib.rs
+++ contracts/{{app_name | kebab_case}}/src/lib.rs
@@ -8,10 +8,10 @@ pub mod state;
 pub use error::{{app_name | upper_camel_case}}Error;
 
 /// The version of your app
-pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const {{app_name | shouty_snake_case}}_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub use contract::interface::{{app_name | upper_camel_case}}Interface;
 
 pub const {{project-name | shouty_snake_case}}_NAMESPACE: &str = "{{project-name}}";
-pub const {{app_name | shouty_snake_case}}_NAME: &str = {{app_name | kebab_case}};
+pub const {{app_name | shouty_snake_case}}_NAME: &str = "{{app_name | kebab_case}}";
 pub const {{app_name | shouty_snake_case}}_ID: &str = const_format::formatcp!("{{project-name | append: app_name | shouty_snake_case}}");
