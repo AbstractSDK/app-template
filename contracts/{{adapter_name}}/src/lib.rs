c3602147871f8e940b223be67bca55bd71e60b74
Buckram <buckram123@gmail.com>
2024-06-03 14:27:28 +0300

    try to fix invalid syntax

--- contracts/{{adapter_name}}/src/lib.rs
+++ contracts/{{adapter_name}}/src/lib.rs
@@ -7,11 +7,12 @@ pub mod state;
 
 pub use contract::interface::{{adapter_name | upper_camel_case}}Interface;
 pub use error::{{adapter_name | upper_camel_case}}Error;
-pub use msg::{{{adapter_name | upper_camel_case}}ExecuteMsg, {{adapter_name | upper_camel_case}}InstantiateMsg};
+pub use msg::{
+    {{adapter_name | upper_camel_case}}ExecuteMsg, {{adapter_name | upper_camel_case}}InstantiateMsg
+};
 
 /// The version of your Adapter
 pub const ADAPTER_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-pub const {{project_name | shouty_snake_case}}_NAMESPACE: &str = "{{project_name}}";
-pub const {{adapter_name | shouty_snake_case}}_NAME: &str = "my-adapter";
-pub const {{adapter_name | shouty_snake_case}}_ID: &str = const_format::formatcp!("{{{project_name | shouty_snake_case}}_NAMESPACE}:{{{adapter_name | shouty_snake_case}}_NAME}");
+pub const {{project_name | shouty_snake_case}}_NAMESPACE: &str = "{{project_name | kebab_case}}";
+pub const {{adapter_name | shouty_snake_case}}_NAME: &str = "{{adapter_name | kebab_case}}";
