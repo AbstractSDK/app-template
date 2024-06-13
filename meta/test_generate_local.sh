ab866662ddc7898f9e1e2b14a149b87d7be80d42
cyberhoward <cyberhoward@protonmail.com>
2024-06-13 12:39:30 +0200

    add genignore and test scripts

---
+++ meta/test_generate_local.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+REPO_ROOT="$(realpath "$(dirname "$0")/..")"
+
+TMP_DIR="$REPO_ROOT/build"
+
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR"
+
+PROJECT_NAME="testgen-local"
+
+(
+  echo "Navigating to $TMP_DIR"
+  cd "$TMP_DIR"
+
+  echo "Generating project from local repository ..."
+  cargo generate --path "$REPO_ROOT" --name "$PROJECT_NAME" --define include_app=true --define include_adapter=true --define adapter_name=test_adapter --define app_name=test_app
+
+  (
+    cd "$PROJECT_NAME"
+    echo "This is what was generated"
+    ls -lA
+
+    # Debug builds first to fail fast
+    echo "Running unit tests ..."
+    cargo unit-test
+    echo "Creating schema ..."
+    cargo schema --package test-app
+    cargo schema --package test-adapter
+
+    echo "Building wasm ..."
+    cargo wasm
+  )
+)
